48b33a0f74a402bde5a6ecd47b7e2183778f4d08
ali amrol <Aliamrollahiii2002@gmail.com>
2025-05-31 17:21:27 +0330

    s16: multi theme

--- app/src/main/java/com/example/digikala/ui/screens/BasketScreen.kt
+++ app/src/main/java/com/example/digikala/ui/screens/BasketScreen.kt
@@ -1,25 +1,67 @@
 package com.example.digikala.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import ir.truelearn.digikala.ui.theme.selectedBottomBar
 
 @Composable
 fun BasketScreen(navController: NavHostController) {
+    if (isSystemInDarkTheme()) {
+        BasketDark()
+    } else {
+        BasketLight()
+    }
+}
+
+@Composable
+fun BasketLight() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Blue),
+            .background(Color.LightGray),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("BasketScreen")
+        Text(
+            "BasketScreen", color = MaterialTheme.colors.selectedBottomBar
+        )
     }
+}
+
+@Composable
+fun BasketDark() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            "BasketScreen", color = MaterialTheme.colors.selectedBottomBar
+        )
+    }
+}
+
+@Composable
+@Preview
+fun BasketLightPreview() {
+    BasketLight()
+}
+
+@Composable
+@Preview
+fun BasketDarkPreview() {
+    BasketDark()
 }
