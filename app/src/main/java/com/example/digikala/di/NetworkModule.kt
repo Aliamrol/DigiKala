1a3746de6b6ce6aca4c0255de01a6893c4af2ea4
ali amrol <Aliamrollahiii2002@gmail.com>
2025-06-01 14:24:46 +0330

    s19: add interceptor to okHttpClient

--- app/src/main/java/com/example/digikala/di/NetworkModule.kt
+++ app/src/main/java/com/example/digikala/di/NetworkModule.kt
@@ -8,6 +8,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -17,28 +18,36 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+
+    @Provides
+    @Singleton
+    internal fun interceptor(): HttpLoggingInterceptor {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return logging
+    }
+
     @Provides
     @Singleton
     fun provideOkHttp(): OkHttpClient =
-        OkHttpClient.Builder()
-            .connectTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
+        OkHttpClient.Builder().connectTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
             .readTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
             .writeTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
+            .addInterceptor(interceptor())
             .build()
 
     @Provides
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
+        Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
 
     @Provides
     @Singleton
-    fun provideApiService(retrofit: Retrofit): ApiInterface =
-        retrofit.create(ApiInterface::class.java
-        )
+    fun provideApiService(retrofit: Retrofit): ApiInterface = retrofit.create(
+        ApiInterface::class.java
+    )
 
 }
