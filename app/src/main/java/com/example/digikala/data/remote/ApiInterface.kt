1ed527cae012c63fa4477878789776b0932ea06d
ali amrol <Aliamrollahiii2002@gmail.com>
2025-06-01 14:19:09 +0330

    s18: add network module

---
+++ app/src/main/java/com/example/digikala/data/remote/ApiInterface.kt
@@ -0,0 +1,7 @@
+package com.example.digikala.data.remote
+
+interface ApiInterface {
+
+    //TODO api calls
+
+}
---
+++ app/src/main/java/com/example/digikala/di/NetworkModule.kt
@@ -0,0 +1,44 @@
+package com.example.digikala.di
+
+import com.example.digikala.data.remote.ApiInterface
+import com.example.digikala.utils.Constants.BASE_URL
+import com.example.digikala.utils.Constants.TIMEOUT_IN_SECONDS
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiInterface =
+        retrofit.create(ApiInterface::class.java
+        )
+
+}
